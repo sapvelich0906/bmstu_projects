e75bd18918e19f9198e102f74afdffa50b6b36a0
sapvelich0906 <sapvelich0906@mail.ru>
2016-10-06 02:53:03 +0600

    Вычисление значения функции по заданному аргументу
    Вычисление по заданному значению аргумента значения функции, содержащей тригонометрическое выражение и выражения с корнями

---
+++ Вычисление значения смешанной функции.cpp
@@ -0,0 +1,21 @@
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	double y;
+	const double pi = 3.14159265;
+	cout << "vvedi x>=5\n";
+	cout << "x=";
+	cin >> x;
+	y = sqrt(pow(x, 3) - 7 * x*x + 14 * x - 8) / sqrt(x - 1) - cos(pi*sqrt(x - 5));
+	cout << "y=" << y << endl;
+	return 0;
+}
+
